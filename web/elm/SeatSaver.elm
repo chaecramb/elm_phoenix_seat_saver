08f483d6da3e3b56abf6881ca97825ade911cac7
Chae Cramb <chaecramb@gmail.com>
2016-09-11 15:08:47 +0100

    creates Elm model

--- web/elm/SeatSaver.elm
+++ web/elm/SeatSaver.elm
@@ -3,7 +3,35 @@ module SeatSaver exposing (..)
 import Html
 
 main =
-  view
+  view init
+
+-- Model
+
+type alias Seat =
+  { seatNo : Into
+  , occupied : Bool
+  }
+
+
+type alias Model =
+  List Seat
+
+
+init =
+  [ { seatNo = 1, occupied = False }
+  , { seatNo = 2, occupied = False }
+  , { seatNo = 3, occupied = False }
+  , { seatNo = 4, occupied = False }
+  , { seatNo = 5, occupied = False }
+  , { seatNo = 6, occupied = False }
+  , { seatNo = 7, occupied = False }
+  , { seatNo = 8, occupied = False }
+  , { seatNo = 9, occupied = False }
+  , { seatNo = 10, occupied = False }
+  , { seatNo = 11, occupied = False }
+  , { seatNo = 12, occupied = False }
+  ]
+  
 
 -- View
 
